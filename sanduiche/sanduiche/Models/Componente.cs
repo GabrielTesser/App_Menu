1667c4f10b07423fe1ed6af3530ef0bf96767306
Usuario <Usuario@CPD02-M12>
2022-09-28 14:44:05 -0300

    Adicionar arquivos de projeto.

---
+++ sanduiche/sanduiche/Models/Componente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sanduiche.Models
+{
+    public class Componente
+    {
+        public string Nome { get; set; }
+        public string CargaHoraria { get; set; }
+        public string AtribuicaoRespobilidade { get; set; }
+        public string ValoresAtitudes { get; set; }
+        public string Competencia { get; set; }
+        public string Habilidade { get; set; }
+        public string BaseTecnologica { get; set; }
+    }
+}
