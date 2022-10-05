c192ae9b416ef9c10cbe51f40ffe7bf5528c1fd5
Gabe_Tesser <95052551+GabrielTesser@users.noreply.github.com>
2022-10-05 14:25:19 -0300

    Add files via upload

--- sanduiche/sanduiche/Views/Primeiro.xaml.cs
+++ sanduiche/sanduiche/Views/Primeiro.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using sanduiche.Models;
+
 namespace sanduiche.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +17,49 @@ namespace sanduiche.Views
         public Primeiro()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Analize de Sistemas",
+                    AtribuicaoRespobilidade = "Análise de sistemas é a atividade que tem como finalidade a realização de estudos de processos a fim de encontrar o melhor caminho racional para que a informação possa ser processada. Os analistas de sistemas estudam os diversos sistemas existentes entre hardwares, softwares e o usuário final.",
+                    ValoresAtitudes = "Estimular a Organização, * Incentivar a Criatividade, * Fortalecer a Percistencia e o Interesse por Situações Problemas",
+                };
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("OPS!", ex.Message, "OK");
+            }
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+
         }
     }
 }
